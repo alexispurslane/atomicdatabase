ab207010e6badbae4ad7eeaa0724859f506b5221
christopherdumas <christopherdumas@gmail.com>
2017-03-25 11:24:41 -0700

    Server working

--- main.js
+++ main.js
@@ -16,3 +16,27 @@ const app = express();
 let db = {};
 let infers = [];
 let facts = [];
+
+app.get('/:name', (req, res, next) => {
+    var options = {
+        root: __dirname + '/public/',
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+
+    var fileName = req.params.name;
+    res.sendFile(fileName, options, (err) => {
+        if (err) {
+            next(err);
+        } else {
+            console.log('served ' + fileName);
+        }
+    });
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
