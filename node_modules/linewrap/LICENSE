cf43ef1c55967f8f9577880beb3650421a162a58
Christopher Dumas <christopherdumas@Christophers-MacBook-Pro.local>
2017-06-19 12:28:21 -0700

    Cleanup

--- node_modules/linewrap/LICENSE
+++
@@ -1,4 +0,0 @@
-Copyright 2013 Feng Qiu (feng@ban90.com)
-
-This project is free software released under the MIT license:
-http://www.opensource.org/licenses/mit-license.php
--- test.js
+++
@@ -1,5 +0,0 @@
-const nlp = require("compromise");
-const process = require("process");
-
-console.log(nlp(process.argv[2]).terms().data().map((d) => d.tags));
-console.log(nlp(process.argv[2]).match(process.argv[3]).found);
