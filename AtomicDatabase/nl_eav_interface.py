1ff2995d54bf8231dde1d6ee8aa13bf6d76946c8
Christopher Dumas <christopherdumas@gmail.com>
2019-04-02 14:02:59 -0700

    parsing eavs done

--- AtomicDatabase/nl_eav_interface.py
+++ AtomicDatabase/nl_eav_interface.py
@@ -47,11 +47,25 @@ def sentence_act_features(word_tokens):
         features['contains({})'.format(word.lower())] = True
     return features
 
+def sanitize_quots(w):
+    (word, tag) = w
+    if tag == "QQ":
+        return "something"
+    else:
+        return word
+
 def ie_preprocess_sent(stop_words, string):
+    reverse = False
+    punc = None
+
     sent = nltk.word_tokenize(string)
-    features = sentence_act_features(sent)
+    reverse = "of" in sent
     sent = [w for w in sent if not w in stop_words]
+
     sent = nltk.pos_tag(sent)
+
+    if sent[-1][1] == ".":
+        punc = sent[-1]
 
     quot_sent = []
     in_quot = False
@@ -62,13 +76,51 @@ def ie_preprocess_sent(stop_words, string):
         elif tag == "''":
             in_quot = False
         elif in_quot and quot_sent[-1][1] == "QQ":
-            quot_sent[-1] = (quot_sent[-1][0] + " " + x, "QQ")
+            leading = ""
+            if len(quot_sent[-1][0]) > 0:
+                leading = quot_sent[-1][0] + " "
+            quot_sent[-1] = (leading + x, "QQ")
         elif tag != "``" and tag != "''" and not in_quot:
             quot_sent.append((x, tag))
+
+    features = sentence_act_features([sanitize_quots(w) for w in quot_sent])
+
+    if reverse:
+        if punc:
+            quot_sent = quot_sent[:-1]
+        quot_sent.reverse()
+        quot_sent.append(punc)
 
     return quot_sent, features
 
 def ie_process_tok(classifier, tagged, features):
     return [(word, c) for (word, c) in tagged if c != '.'], classifier.classify(features)
 
-def convert_to_predicate(words, stype):
+def ie_process_eav(words):
+    entity = None
+    attribute = None
+    value = None
+    for (word, tag) in words:
+        if not entity and (tag == "QQ" or tag == "NNP" or tag == "NN"):
+            entity = word
+        elif not attribute and ("NN" in tag):
+            attribute = word
+        elif not value and attribute and entity:
+            value = word
+
+    try:
+        value = float(value)
+    except ValueError:
+        pass
+    return (entity.replace(" ", ""), attribute, value)
+
+RULE = 1
+NEW_DATA = 2
+UNKNOWN = 3
+def ie_convert_dispatch(words, stype):
+    if stype == "Clarify" or "Answer" in stype or stype == "Statement":
+        return NEW_DATA, ie_process_eav(words)
+    elif "Question" in stype:
+        return RULE, ie_process_rule(words)
+    else:
+        return UNKNOWN, None
