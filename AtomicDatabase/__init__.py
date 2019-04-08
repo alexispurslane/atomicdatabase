28ab47d19aa0acf01f4568b15b77a3d2df173e36
Christopher Dumas <christopherdumas@gmail.com>
2019-04-07 19:37:36 -0700

    basic operator stuff done, but now simplequeries don't parse lol. Rule conversion not implemented

--- AtomicDatabase/__init__.py
+++ AtomicDatabase/__init__.py
@@ -1,1 +1,4 @@
 """A EAV, logic programming and natural-language based, rules-aware database proof-of-concept."""
+import eav_database
+import nl_eav_interface
+
--- AtomicDatabase/nl_eav_interface.py
+++ AtomicDatabase/nl_eav_interface.py
@@ -15,11 +15,11 @@ PATTERNS = {
         {'DEP': 'pobj'},
     ],
     "Predicate": [
-        {'POS': 'NOUN'},
+        {'POS': {'IN': ['NOUN', 'PROPN']}},
         {'POS': 'PART', 'OP': '?'},
         {'DEP': 'nsubj'},
         {'LEMMA': 'be'},
-        {'DEP': 'attr'},
+        {'DEP': 'attr'}
     ],
     "SimpleQuery": [
         {'TAG': 'WP'},
@@ -58,31 +58,96 @@ def create_text_entities(string):
     return new_str, entities
 
 def get_matches(matches, doc):
-    return [(nlp.vocab.strings[match_id], doc[start:end]) for match_id, start, end in matches]
+    return [(doc.vocab.strings[match_id], doc[start:end]) for match_id, start, end in matches]
+
+def access_repeat(a, l):
+     intermediate = a
+     for i in l:
+         intermediate = intermediate[i]
+     return intermediate
 
 def group_conjs(strings):
-    collections = [['and']]
-    current_conj = 'and'
-    for s in strings:
-        s_s = s.strip()
-        print("CONJ: " + str(current_conj))
-        print("STR: " + s)
-        if s_s == 'and' or s_s == 'or':
-            if current_conj != s_s:
-                collections.append([s_s])
-                current_conj = s_s
+    paren_pos_stack = [0]
+    output = []
+    idx = 0
+    for s in reversed([s.strip() for s in strings if len(s.strip()) > 0]):
+        if s == 'then':
+            s = 'and'
+        if s in ['and', 'or']:
+            if not (output[paren_pos_stack[-1]] in ['and', 'or']):
+                output.insert(paren_pos_stack[-1], s)
+                idx += 1
+            elif output[paren_pos_stack[-1]] != s:
+                paren_pos_stack.pop()
+                output.append(')')
+                output.append('(')
+                output.append(s)
+                paren_pos_stack.append(idx - 1)
+                idx += 3
+        elif s == ')':
+            output.append('(')
+            paren_pos_stack.append(idx + 1)
+            idx += 1
+        elif s == '(':
+            output.append(')')
+            paren_pos_stack.pop()
+            idx += 1
         else:
-            collections[-1].append(s_s)
-    return collections
+            output.append(s)
+            idx += 1
 
-def understand_predicate(matcher, string):
+    sublisted_out = []
+    in_paren = False
+    current_point = []
+    for out in reversed(output):
+        if out == ')':
+            access_repeat(sublisted_out, current_point).append([])
+            current_point.append(len(access_repeat(sublisted_out, current_point)) - 1)
+        elif out == '(':
+            current_point.pop()
+        else:
+            if out == 'and':
+                access_repeat(sublisted_out, current_point).insert(0, '&')
+            elif out == 'or':
+                access_repeat(sublisted_out, current_point).insert(0, '|')
+            else:
+                access_repeat(sublisted_out, current_point).append(out)
+    return sublisted_out
+
+def recursive_map(constarg, fun, lst):
+    # need multiple lines of the function inside the map, but the function
+    # inside the map has to have access to this one. Hence, reimplement map
+    res = []
+    for item in lst:
+        if isinstance(item, list):
+            res.append(recursive_map(constarg, fun, item))
+        else:
+            mat = fun(constarg, item)
+            if len(mat) > 0:
+                res.append(mat)
+            else:
+                res.append(item)
+    return res
+
+def run_nlp(constarg, string):
+    (matcher, nlp) = constarg
+    doc = nlp(string)
+    return get_matches(matcher(doc), doc)
+
+def understand_predicate(nlp, matcher, string):
     string, entities = create_text_entities(string)
-    conjugation_groups = group_conjs(re.split(" (and|or) ", string))
+    conjugation_groups = group_conjs(re.split("( and | then | or |[()])", string))
 
-    new_groups = []
-    for group in conjugation_groups:
-        new_groups.append([group[0]])
-        for line in group[1:]:
-            doc = nlp(line)
-            new_groups[-1].append(get_matches(matcher(doc), doc))
-    return new_groups, entities
+    # No multiline lambdas means no closures, so i have to implement them manually
+    return recursive_map((matcher, nlp), run_nlp, conjugation_groups), entities
+
+def pos_printer(doc):
+    columns = ["TEXT", "LEMMA", "POS", "TAG", "DEP"]
+    data = [[el.text, el.lemma_, el.pos_, el.tag_, el.dep_] for el in doc]
+
+    row_format ="{:>15}" * (len(columns) + 1)
+    print(row_format.format("", *columns))
+    for row in data:
+        print(row_format.format("", *row))
+
+
